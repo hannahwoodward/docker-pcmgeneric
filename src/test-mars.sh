f011e809a0c9bab7b59a11d230bd9096745490ef
Hannah Woodward <hannah@woodward.sh>
2024-01-09 17:05:52 +0000

    Add test model progress to test-mars.sh

--- src/test-mars.sh
+++ src/test-mars.sh
@@ -6,6 +6,7 @@ mkdir -p $HOME/runs
 
 # Compile model
 cd $MODELDIR
+echo "Compiling PCM"
 ./makelmdz_fcm -arch local -p std -d 32x32x15 -b 32x36 -j 4 gcm > makelmdz_fcm.out 2>&1
 
 if [ ! -e "bin/$MODEL" ]
@@ -15,6 +16,7 @@ if [ ! -e "bin/$MODEL" ]
     fi
 
 cd $HOME/runs
+echo "Downloading config $PLANET_CONFIG"
 wget -nv --no-check-certificate "https://web.lmd.jussieu.fr/~lmdz/planets/generic/$PLANET_CONFIG.tar.gz"
 
 tar xvzf "$PLANET_CONFIG.tar.gz"
@@ -26,4 +28,5 @@ echo "source $MODELDIR/arch.env" > run_gcm.sh
 echo "./$MODEL > gcm.out 2>&1" >> run_gcm.sh
 chmod u=rwx run_gcm.sh
 
+echo "Running $PLANET_CONFIG with $MODEL"
 ./run_gcm.sh
